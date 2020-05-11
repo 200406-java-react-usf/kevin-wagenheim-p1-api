513c8daf34addc68aadceae4b1e6ac1da9f3ffa5
Wagenheim <wagenheimk@me.com>
2020-05-10 22:29:41 -0400

    set up connection to the newly created DB for p1

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,19 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import {Pool} from 'pg';
+
+dotenv.config();
+
+export const connectionPool: Pool =  new Pool({
+
+    host: process.env['DB_HOST'],
+    port: +process.env['DB_PORT'],
+    database: process.env['DB_NAME'],
+    user: process.env['DB_USERNAME'],
+    password: process.env['DB_PASSWORD'],
+    max: 5
+
+});
 
 let app = express();
 
