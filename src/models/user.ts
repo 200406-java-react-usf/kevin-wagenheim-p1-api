6d133bd6aef9f43251429206db2caf33937a2f96
Wagenheim <wagenheimk@me.com>
2020-05-11 22:03:15 -0400

    created the user model

---
+++ src/models/user.ts
@@ -0,0 +1,25 @@
+import { timingSafeEqual } from "crypto";
+export class User {
+
+    id: number;
+    username: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    roleId: number;
+
+    constructor(id: number, un: string, pw: string, fn: string, ln: string, email: string, roleId: number){
+
+        this.id = id;
+        this.username = un;
+        this.password = pw;
+        this.firstName = fn;
+        this.lastName = ln;
+        this.email = email;
+        this.roleId = roleId;
+
+    }
+
+
+}
