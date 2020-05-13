4d77c6bf6b940a93f32b3478a40cd5c23cd2b97e
Wagenheim <wagenheimk@me.com>
2020-05-12 22:07:00 -0400

    added methods up to getbyuniquekey

--- src/repos/user-repo.ts
+++ src/repos/user-repo.ts
@@ -1,6 +1,7 @@
 import {User} from '../models/user.js';
 import { PoolClient } from 'pg';
 import { connectionPool } from '../index.js';
+import { InternalServerError } from '../errors/errors.js';
 
 export class UserRepository {
 
@@ -14,7 +15,41 @@ export class UserRepository {
             let rs = await client.query(sql);
             return rs.rows;
         } catch(e){
-            throw new Error();
+            throw new InternalServerError();
+        } finally{
+            client && client.release();
+        }
+
+    }
+
+    async getById(id: number): Promise<User>{
+
+        let client: PoolClient;
+
+        try{
+            client = await connectionPool.connect();
+            let sql = 'select * from app_users where user_id = $1'
+            let rs = await client.query(sql, [id]);
+            return rs.rows[0];
+        } catch (e){
+            throw new InternalServerError();
+        } finally{
+            client && client.release();
+        }
+
+    }
+
+    async getByUniqueKey(key: string, val: string): Promise<User> {
+
+        let client: PoolClient;
+
+        try{
+            client = await connectionPool.connect();
+            let sql = `select * from app_users where ${key} = $1`;
+            let rs =  await client.query(sql, [val]);
+            return rs.rows[0]; 
+        } catch(e){
+            throw new InternalServerError();
         } finally{
             client && client.release();
         }
