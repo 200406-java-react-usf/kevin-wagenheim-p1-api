146dbb0d91968698f33f5cff5bb1c3cfb0bfbcaf
Wagenheim <wagenheimk@me.com>
2020-05-13 17:18:22 -0400

    initialized reimb repo/services in app config

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -1,9 +1,15 @@
 import { UserRepository } from "../repos/user-repo";
 import { UserService } from "../services/user-services";
+import { ReimbRepository } from "../repos/reimb-repo";
+import { ReimbServices } from "../services/reimb-services";
 
 const userRepo = new UserRepository();
 const userService = new UserService(userRepo);
 
+const reimbRepo = new ReimbRepository();
+const reimbService = new ReimbServices(reimbRepo);
+
 export default {
-    userService
+    userService,
+    reimbService
 }
