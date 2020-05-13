5a37c8007471db64f72bc707cf587ecefb9f214c
Wagenheim <wagenheimk@me.com>
2020-05-12 22:10:43 -0400

    added in id routers

--- src/routers/user-router.ts
+++ src/routers/user-router.ts
@@ -1,14 +1,28 @@
 import express from 'express';
 import {UserRepository} from '../repos/user-repo';
+import appConfig from '../config/app-config';
 
 export const UserRouter = express.Router();
 
-let userRepo = new UserRepository;
+let userService = appConfig.userService;
 
 UserRouter.get('', async (req, resp) => {
 
     try{
-        let pl = await userRepo.getAll();
+        let pl = await userService.getAllUsers();
+        resp.status(200).json(pl);
+    } catch(e){
+        resp.status(200).json(e);
+    }
+
+});
+
+UserRouter.get('/id/:id', async (req, resp) => {
+
+    let id = +req.params.id;
+
+    try{
+        let pl = await userService.getUserById(id);
         resp.status(200).json(pl);
     } catch(e){
         resp.status(200).json(e);
