34ca6aada00dcf43dfd0c8416301ea8a02d8600f
Wagenheim <wagenheimk@me.com>
2020-05-13 20:59:27 -0400

    added logic for reimb repo and services, need to test all these

---
+++ src/services/reimb-services.ts
@@ -0,0 +1,143 @@
+import { ReimbRepository } from "../repos/reimb-repo";
+import { Reimbursments } from "../models/reimb";
+import { ResourceNotFoundError, InvalidInputError, ResourceConflictError } from "../errors/errors";
+import { isValidId, isEmptyObject, isPropertyOf, isValidString, isValidObject } from "../util/validators";
+
+export class ReimbServices{
+
+    constructor(private reimbRepo: ReimbRepository){
+
+        this.reimbRepo = reimbRepo;
+
+    }
+
+    async getAllReimbs(): Promise<Reimbursments[]>{
+
+        let result = await this.reimbRepo.getAll();
+
+        if(result.length === 0){
+            throw new ResourceNotFoundError('No Reimbursments found in the database');
+        }
+
+        return result;
+
+    }
+
+    async getReimbById(id: number): Promise<Reimbursments>{
+
+        if (!isValidId(id)){
+            throw new InvalidInputError('Invalid ID was input.');
+        }
+
+        let result = await this.reimbRepo.getById(id);
+
+        if (!isEmptyObject(result)){
+            throw new ResourceNotFoundError('No reimbursment with that ID was found');
+        }
+
+        return result;
+
+    }
+
+    async getReimbByUniqueKey(queryObj: any): Promise<Reimbursments>{
+
+        try{
+
+            let queryKeys = Object.keys(queryObj);
+
+            if(!queryKeys.every(key => isPropertyOf(key, Reimbursments))){
+                throw new InvalidInputError('Key is not a property of a Reimbursments');
+            }
+
+            let key = queryKeys[0];
+            let val = queryObj[key];
+
+            if(key === 'id'){
+                return await this.getReimbById(val);
+            }
+
+            if(!isValidString(val)){
+                throw new InvalidInputError('Value is not a string');
+            }
+
+            let reimb = await this.reimbRepo.getByUniqueKey(key, val);
+
+            if(!isEmptyObject(reimb)){
+                throw new ResourceNotFoundError('No reimbursment found with given properties');
+            }
+
+            return reimb;
+
+        } catch(e){
+            throw e;
+        }
+
+    }
+
+    async addNewReimb(newReimbursment: Reimbursments): Promise<boolean>{
+
+        try{
+
+            if(!isValidObject(newReimbursment, 'id')){
+                throw new InvalidInputError('Invalid Reimbursment was input')
+            }
+
+            await this.reimbRepo.save(newReimbursment);
+
+            return true;
+
+        } catch(e){
+            throw e;
+        }
+
+    }
+
+    async updateReimb(updatedReimb: Reimbursments): Promise<boolean>{
+
+        try{
+
+            if(!isValidObject(updatedReimb, 'id') || !isValidId(updatedReimb.id)){
+                throw new InvalidInputError('Invalid Reimbursment was input');
+            }
+
+            let reimbToUpdate = await this.getReimbById(updatedReimb.id);
+
+            if(updatedReimb.reimbStatusId !== 1){
+                throw new ResourceConflictError('Cannot update a non-pending Reimbursment');
+            }
+
+            if(updatedReimb.authorId !== reimbToUpdate.authorId){
+                throw new ResourceConflictError('Cannot update author ID');
+            }
+
+            if(updatedReimb.id !== reimbToUpdate.id){
+                throw new ResourceConflictError('Cannot update Reimbursment ID');
+            }
+
+            if(updatedReimb.reimbStatusId !== reimbToUpdate.reimbStatusId){
+                throw new ResourceConflictError('Cannot update status of Reimbursment');
+            }
+
+            if(updatedReimb.resolved !== reimbToUpdate.resolved){
+                throw new ResourceConflictError('Cannot update resolved time');
+            }
+
+            if(updatedReimb.resolverId !== reimbToUpdate.resolverId){
+                throw new ResourceConflictError('Cannot update resolver');
+            }
+
+            if(updatedReimb.submitted !== reimbToUpdate.submitted){
+                throw new ResourceConflictError('Cannot update submitted time');
+            }
+
+            await this.reimbRepo.update(updatedReimb);
+
+            return true;
+
+        } catch(e){
+            throw e;
+        }
+
+    }
+
+}
