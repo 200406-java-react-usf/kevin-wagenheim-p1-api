f5c82e610eb495b230917b8801e55e235c234c87
Wagenheim <wagenheimk@me.com>
2020-05-17 16:30:27 -0400

    gave wrong link, need to change to s3 link

--- src/middleware/cors-filter.ts
+++ src/middleware/cors-filter.ts
@@ -2,7 +2,8 @@ import {Request, Response} from 'express';
 
 export function corsFilter(req: Request, resp: Response, next){
 
-    resp.header('Access-Control-Allow-Origin', 'http://localhost:3000'); 
+    //resp.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    resp.header('Access-Control-Allow-Origin', 'http://kevinwagenheimproject1api-env.eba-pfzxxg4c.us-east-1.elasticbeanstalk.com'); 
     resp.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
     resp.header('Access-Control-Allow-Credentials', 'true');
     resp.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
